d3efe4736a7e428a676fb2d0dd4404e785ce1678
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-05-16 22:34:29 +0200

    Ready

--- src/utils/combiner/Combiner.js
+++ src/utils/combiner/Combiner.js
@@ -54,6 +54,7 @@ export const combine = (items, requiredWidth, requiredHeight, backgroundIndex = 
 
     const defs = `
         <defs>
+            <clipPath id="clip-shape"></clipPath>
             <style>
                 ${Array.from(combinedStyles).join("\n")}
             </style>
@@ -74,3 +75,82 @@ export const insertPngIntoSvg = (svg, png) => {
     return svg.slice(0, insertIndex) + injected + svg.slice(insertIndex);
 };
 
+export const combineHorizontally = (items, requiredWidth, requiredHeight) => {
+        let combinedContent = "";
+        let combinedStyles = new Set();
+
+        if (!items || !Array.isArray(items)) return "";
+
+        for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+            const item = items[itemIndex];
+
+            const svg = item.replace(/cls-/g, `item-${itemIndex}-cls-`);
+            const styles = extractSvgStyles(svg);
+            if (styles) combinedStyles.add(styles);
+
+            const content = extractSvgContent(svg);
+            const offsetX = (itemIndex) * requiredWidth
+
+            combinedContent += `
+            <g transform="translate(${offsetX}, 0)">
+                ${content}
+            </g>
+        `;
+
+        }
+
+        const defs = `
+        <defs>
+            <style>
+                ${Array.from(combinedStyles).join("\n").replace(/<\/?style>/g, '')};.
+            </style>
+        </defs>
+    `;
+
+        return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="${requiredWidth * items.length}" height="${requiredHeight}" viewBox="0 0 ${requiredWidth * items.length} ${requiredHeight}">
+            ${defs}
+            ${combinedContent}
+        </svg>`;
+    }
+;
+
+export const combineVertically = (items, requiredWidth, requiredHeight) => {
+        let combinedContent = "";
+        let combinedStyles = new Set();
+
+        if (!items || !Array.isArray(items)) return "";
+
+        for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+            const item = items[itemIndex];
+
+            const svg = item.replace(/cls-/g, `item-${itemIndex}-cls-`);
+            const styles = extractSvgStyles(svg);
+            if (styles) combinedStyles.add(styles);
+
+            const content = extractSvgContent(svg);
+            const offsetY = (itemIndex) * requiredHeight
+
+            combinedContent += `
+            <g transform="translate(0, ${offsetY})">
+                ${content}
+            </g>
+        `;
+
+        }
+
+        const defs = `
+        <defs>
+            <style>
+                ${Array.from(combinedStyles).join("\n").replace(/<\/?style>/g, '')};.
+            </style>
+        </defs>
+    `;
+
+        return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="${requiredWidth * items.length}" height="${requiredHeight}" viewBox="0 0 ${requiredWidth * items.length} ${requiredHeight}">
+            ${defs}
+            <rect height="${requiredHeight}" width="${requiredWidth}" rx="20" ry="20">
+                ${combinedContent}
+            </rect>
+        </svg>`;
+    }
+;
