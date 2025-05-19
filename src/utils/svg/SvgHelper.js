bf9cb9f5cb8ab4341e8633bd74c161e75db49acd
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-05-19 15:34:54 +0200

    v1.2.1

--- src/utils/svg/SvgHelper.js
+++ src/utils/svg/SvgHelper.js
@@ -69,8 +69,8 @@ export const toPngSrc = async (svgString) => {
     }
 
 
-    const cropWidth = (topVisibleRow < 200) ? 404 : 343.4;
-    const cropHeight = (topVisibleRow < 200) ? 404 : 343.4;
+    const cropWidth = (topVisibleRow < 214.8) ? 404 : 325.2;
+    const cropHeight = (topVisibleRow < 214.8) ? 404 : 325.2;
     const cropX = Math.max(0, Math.floor((width - cropWidth) / 2));
     let cropY = topVisibleRow;
     if (cropY + cropHeight > height) {
