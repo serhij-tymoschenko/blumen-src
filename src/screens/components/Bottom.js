d296583e70f75568e2e3f5928e3ad60fbdc3aad1
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-05-19 11:16:44 +0200

    Update

--- src/screens/components/Bottom.js
+++ src/screens/components/Bottom.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Stack, Link, Typography} from '@mui/material';
+import {Link, Stack} from '@mui/material';
+import bmc from "../../bmc-yellow-button.png";
 
 const Bottom = () => {
     return (
@@ -10,11 +11,10 @@ const Bottom = () => {
             justifyContent="flex-end"
             width={'100vw'}
         >
-            <Typography variant="h5">
-                <Link href="https://buymeacoffee.com/tymoschenko" target="_blank" rel="noopener">
-                    Buy me a coffee
-                </Link>
-            </Typography>
+            <Link href="https://buymeacoffee.com/tymoschenko" target="_blank" rel="noopener">
+                {/* eslint-disable-next-line jsx-a11y/alt-text */}
+                <img src={bmc} width={182} height={51}/>
+            </Link>
         </Stack>
     );
 };
