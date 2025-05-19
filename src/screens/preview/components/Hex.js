67c37d05eb54d5639870df39461be71dab18f1f6
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-05-19 20:39:57 +0200

    v1.3.3

--- src/screens/preview/components/Hex.js
+++ src/screens/preview/components/Hex.js
@@ -2,10 +2,10 @@ import {toSvgFile} from "../../../utils/svg/SvgHelper";
 
 const Hex = ({hex}) => {
     return (
-        <div style={{position: "relative", width: 120, height: 128.4}}>
+        <div style={{position: "relative", width: 120, height: 125.4}}>
             <img
                 width={120}
-                height={128.4}
+                height={125.4}
                 src={toSvgFile(hex)}
             />
         </div>
